cb855e109d09e3627f43d8ebb2413f5d56141d75
strbytes <strbytes@gmail.com>
2022-05-27 14:28:11 -0400

    Get expr.py into working order

--- expr.py
+++ expr.py
@@ -6,31 +6,34 @@ class Expression:
         raise NotImplementedError
 
     def __repr__(self):
-        arguments = "(" + ", ".join([str(arg) for arg in self.args]) + ")"
+        arguments = "(" + ", ".join([repr(arg) for arg in self.args]) + ")"
         return type(self).__name__ + arguments
 
 
 class BinaryExpr(Expression):
     def __init__(self, left, operator, right):
+        Expression.__init__(self, left, operator, right)
         self.left = left
         self.operator = operator
         self.right = right
 
     def eval(self):
-        return GLOBALS[self.operator](self.left, self.right)
+        return GLOBALS[self.operator](self.left.eval(), self.right.eval())
 
 
-class UnaryExpr(Expression):
+class CallExpr(Expression):
     def __init__(self, operator, operand):
+        Expression.__init__(self, operator, operand)
         self.operator = operator
         self.operand = operand
 
     def eval(self):
-        return GLOBALS[self.operator](self.operand)
+        return GLOBALS[self.operator](self.operand.eval())
 
 
 class Literal(Expression):
     def __init__(self, value):
+        Expression.__init__(self, value)
         self.value = value
 
     def eval(self):
@@ -41,11 +44,11 @@ from operator import add, sub, mul, truediv
 from math import log, sqrt
 
 GLOBALS = {
-    "add": add,
-    "sub": sub,
-    "mul": mul,
-    "div": truediv,
-    "pow": pow,
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": truediv,
+    "^": pow,
     "log": log,
     "sqrt": sqrt,
     "neg": lambda x: -x,
