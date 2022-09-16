4a5561df9eeac59c2663982815f433f825a32c0c
strbytes <strbytes@gmail.com>
2022-09-16 11:45:00 -0400

    Add expr.js (Expression objects)

---
+++ expr.js
@@ -0,0 +1,84 @@
+const Globals = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "*": (x, y) => x * y,
+    "/": (x, y) => x / y,
+    "^": Math.pow,
+    "log": Math.log,
+    "sqrt": Math.sqrt,
+    "neg": (x) => -x,
+    "pi": Math.PI,
+    "e": Math.E,
+}
+
+class Expression {
+  #args;
+  constructor(...args) {
+    this.#args = args;
+  }
+  
+  eval() {
+    throw "Superclass method not implemented"
+  }
+  
+  toString() {
+    return this.constructor.name + "(" + this.#args + ")";
+  }
+}
+
+class BinaryExpr extends Expression {
+  #left;
+  #operator;
+  #right;
+
+  constructor(left, operator, right) {
+    super();
+    this.#left = left;
+    this.#operator = operator;
+    this.#right = right;
+  }
+  
+  eval() {
+    let left = this.#left.eval();
+    let operator = Globals[this.#operator];
+    let right = this.#right.eval();
+    return operator(left, right);
+  }
+}
+
+class CallExpr extends Expression {
+  #operator;
+  #operand;
+  
+  constructor(operator, operand) {
+    super();
+    this.#operator = operator;
+    this.#operand = operand;
+  }
+  
+  eval() {
+    let operator = Globals[this.#operator];
+    let operand = this.#operand.eval();
+    return operator(operand);
+  }
+}
+
+class Literal extends Expression {
+  #value;
+  
+  constructor(value) {
+    super();
+    this.#value = value;
+  }
+  
+  eval() {
+    if (typeof this.#value == 'number') {
+      return this.#value;
+    } else {
+      return Globals[this.#value];
+    }
+  }
+}
+
+module.exports = {BinaryExpr, CallExpr, Literal};
+
