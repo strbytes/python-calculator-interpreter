086f9d101b7349e74aef5848e11d93028f2bf6d6
strbytes <strbytes@gmail.com>
2022-09-17 11:06:19 -0400

    Add calc.js, gitignore, node package info
    Using readline-sync for the REPL implementation, may switch to
    prompt-sync to see if its history feature works.
    
    TODO implement implicit multiplication. Was originally in the token
    buffer in the Python version, this may still make sense. The current
    implementation eats everything after an implied multiplication.

---
+++ calc.js
@@ -0,0 +1,17 @@
+const read = require('./reader');
+const readlineSync = require('readline-sync');
+
+function repl() {
+  while (true) {
+    try {
+      console.log(read(readlineSync.question("> ")).eval());
+    } catch (e) {
+        console.log(e);
+        return;
+    }
+  }
+}
+
+if (require.main === module) {
+  repl();
+}
